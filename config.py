a3734d22804c463520f6c050ddbd16ba053f41a0
Taras Rodionov <tarasrodionov@gmail.com>
2020-03-03 20:42:08 +0300

    Deploy

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ DEBUG = False
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 REDIS_URL = os.environ.get("REDIS_URL")
-MONGO_URI = os.environ.get("MONGO_URI")
+MONGO_URI = os.environ.get("MONGODB_URI")
 DOTA_API = os.environ.get("DOTA_API")
 MONGO_DB = MONGO_URI.split('/')[-1]
